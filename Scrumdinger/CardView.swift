921af0280a0af71e79e13656780272e2648503e7
Steffen Suess <steffen@steffensuess.com>
2022-10-09 11:46:15 +0200

    Displaying data in a list

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -34,7 +34,7 @@ struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.sampleData[0]
     static var previews: some View {
         CardView(scrum: scrum)
-            .previewLayout(.fixed(width: 400, height: 60))
             .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
     }
 }
